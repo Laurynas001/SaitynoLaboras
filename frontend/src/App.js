0a6799a2a5c0263966a6ae8902748cf03ffff666
LaurynasLiuiza <laurynas.liuiza@ktu.edu>
2020-11-22 13:25:32 +0200

    addedLoginPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import Navbar from './components/Navbar/Navbar';
 import About from './components/About/About';
 import GasStations from './components/GasStation/GasStations';
 import Prices from './components/Prices/Prices';
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -3,7 +3,24 @@ import './Login.css';
 
 function Login() {
     return (
-        <div></div>
+        <div className='parentDiv'>
+            <div className='loginComponent'>
+                <div className='title'>Prisijungimas</div>
+                <div className='outterUsername'>
+                    <i className="fas fa-user-alt"/>
+                    <input type='text' placeholder='Naudotojo vardas' className='usernameText'/>
+                </div>
+                <div className='outterPassword'>
+                    <i className="fas fa-lock" />
+                    <input type='password' placeholder='Slaptažodis' className='passwordText' />
+                </div>
+                <div className='outterButton'>
+                    <div className='innerButton'>
+                        <button className='loginButton1'>Prisijunk</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
